78a27d6c31afe483bb8454376fce94d7c5b9ff8e
Heman Kornbluh <knaftoli@gmail.com>
2023-06-26 14:22:53 +0300

    main app navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,36 @@
 <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  class="h-full bg-gray-50">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @wireUiScripts
+    @livewireStyles
+</head>
+
+<body class="h-full">
+
+    <div >
+
+        <x-navigation-menu />
+        <main class="py-10 lg:pl-48">
+            <div class="px-4 sm:px-6 lg:px-8">
+                {{-- <!-- Your content --> --}}
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
+    @livewireScripts
+</body>
+
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -42,4 +74,4 @@
 
         @livewireScripts
     </body>
-</html>
+</html> --}}
