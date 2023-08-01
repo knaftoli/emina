b920e581186075ba1efce66a00bfe00d7af2d9fb
Heman Kornbluh <knaftoli@gmail.com>
2023-08-01 14:01:28 +0300

    WIP

--- app/Http/Livewire/PropertyListing/Buttons/Scrape.php
+++ app/Http/Livewire/PropertyListing/Buttons/Scrape.php
@@ -11,7 +11,7 @@ use RoachPHP\Roach;
 class Scrape extends Component
 {
     function scrape() {
-        set_time_limit(300);
+        set_time_limit(900);
         Roach::startSpider(RightMoveSpider::class);
     }
 
