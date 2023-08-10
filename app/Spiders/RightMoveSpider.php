e2b80ac6857c82740393dd66da3ca426d6e30d9d
Heman Kornbluh <knaftoli@gmail.com>
2023-08-10 16:25:37 +0300

    WIP

--- app/Spiders/RightMoveSpider.php
+++ app/Spiders/RightMoveSpider.php
@@ -37,6 +37,7 @@ class RightMoveSpider extends BasicSpider
         'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=POSTCODE%5E746788&radius=40.0&includeSSTC=false&maxDaysSinceAdded=1',
         'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=POSTCODE%5E4173320&radius=40.0&includeSSTC=false&maxDaysSinceAdded=1',
         'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=POSTCODE%5E160519&radius=40.0&includeSSTC=false&maxDaysSinceAdded=1',
+        'https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=POSTCODE%5E814297&radius=40.0&includeSSTC=false&maxDaysSinceAdded=1',
     ];
 
     public array $downloaderMiddleware = [
