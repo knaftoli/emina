65688db51ec530aa9ad363cb4e26627da5fbf26d
Heman Kornbluh <knaftoli@gmail.com>
2023-08-15 14:03:38 +0300

    WIP

--- app/Spiders/RightMoveSpider.php
+++ app/Spiders/RightMoveSpider.php
@@ -136,6 +136,15 @@ class RightMoveSpider extends BasicSpider
 
             yield $this->item($this->compactListing($response, $search, $relevant));
         }elseif(
+            Str::of($text)->contains('structural') ||
+            Str::of($text)->contains('Structural') ||
+            Str::of($text)->contains('STRUCTURAL')
+        ){
+            $search = 'Structural';
+            $relevant = true;
+
+            yield $this->item($this->compactListing($response, $search, $relevant));
+        }elseif(
             Str::of($text)->contains('short lease') ||
             Str::of($text)->contains('Short lease') ||
             Str::of($text)->contains('Short Lease') ||
