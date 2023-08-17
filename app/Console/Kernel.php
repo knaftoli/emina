84d7661dd0371ac12e2101e8ea5d05d2efb792f0
Heman Kornbluh <knaftoli@gmail.com>
2023-08-17 17:53:02 +0300

    WIP

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:scrape')->hourlyAt(13);
+        $schedule->command('app:scrape')->hourlyAt(47);
         $schedule->command('app:delete-listing')->hourlyAt(37);
     }
 
